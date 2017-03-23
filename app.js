16029626f7abc5414a4ace56b8a31f73b709803f
tomasz wojda <woydatomasz@gmail.com>
2017-03-23 23:59:28 +0100

    front page prawie gotowy

--- app.js
+++ app.js
@@ -5,5 +5,39 @@ $(document).ready(function(){
         $('.headMenu').toggleClass('isActive')
     });
     
+    $('*[data-animate]').addClass('hide').each(function(){
+        $(this).viewportChecker({
+            classToAdd: 'show animated ' + $(this).data('animate'),
+            classToRemove: 'hide',
+            offset: '30%'
+        });
+    });
+    
+    $(".headLogoSpan").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".mors").offset().top
+        }, 1200);
+    });
+    
+    var article = $('.showA');
+    var vheight = $('divArticle').outerHeight();
+    
+    
+        article.on('click', function(){
+            $('article.showB').toggle('.hide');  
+        });  
+    
+    
+    var articleB = $('article.showB');
+    
+    articleB.each(function(){
+        $(this).on('click', function(){
+            
+            $(this).siblings().toggleClass('hide');
+        });  
+    })
+    
+  
+   
     
 });
