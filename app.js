c9d9b552d5850e0a5e13e359ca5cc3bd9a5665fb
tomasz wojda <woydatomasz@gmail.com>
2017-03-24 15:55:04 +0100

    zrobiona część jsa

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
     
-    $('.headBurger').on('click',function(){
+    var headBurger = $('.headBurger')
+    var headMenuLi = $('.headMenuLi');
+    var headMenu = $('.headMenu');
+    
+    headBurger.on('click',function(){
         $(this).toggleClass('isActive');       
         $('.headMenu').toggleClass('isActive')
+        })
+        $('a[href^="#"]').click(function() {
+            var target = this.hash,
+                $target = $(target);
+            
+            $('html, body').animate({
+                scrollTop: $target.offset().top-50
+            }, 1200);
+        headMenu.toggleClass('isActive')
+       
+        headBurger.toggleClass('isActive')
+       
     });
     
+  
+
     $('*[data-animate]').addClass('hide').each(function(){
         $(this).viewportChecker({
             classToAdd: 'show animated ' + $(this).data('animate'),
@@ -19,25 +37,34 @@ $(document).ready(function(){
         }, 1200);
     });
     
-    var article = $('.showA');
-    var vheight = $('divArticle').outerHeight();
-    
-    
-        article.on('click', function(){
-            $('article.showB').toggle('.hide');  
-        });  
-    
+   
     
     var articleB = $('article.showB');
     
-    articleB.each(function(){
-        $(this).on('click', function(){
-            
-            $(this).siblings().toggleClass('hide');
-        });  
-    })
+    
+        articleB.on('click', function(){
+            console.log(articleB);
+    
+            articleB.hide();
+            $(this).show().toggleClass('article').toggleClass('articleZoom');
+        
+            var self = this;
+            setTimeout(function(){
+            if(!$(self).hasClass('articleZoom')){
+               articleB.show(); 
+            };
+            },3000)
+           
+        }); 
+    
+    
+    
+    
+
     
   
    
     
 });
+
+
